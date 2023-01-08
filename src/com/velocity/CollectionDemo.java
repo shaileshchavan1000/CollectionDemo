54edd0d0978455703f08ea1e7fbf2bbe336aca21
shaileshchavan <shaileshchavan1000@gmail.com>
2023-01-08 16:35:44 +0530

    first commit : code

---
+++ src/com/velocity/CollectionDemo.java
@@ -0,0 +1,14 @@
+package com.velocity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectionDemo {
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<String>();
+		list.add("Shailesh");
+		list.add("hariom");
+		System.out.println(list);
+	}
+
+}
